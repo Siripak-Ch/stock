cfd05ac7af80449d951351999493666b0579e4b5
Siripak-Ch <siripak.chat@gmail.com>
2026-07-21 00:11:19 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 
 window.CES_CONFIG = {
   // IMPORTANT: After deploying Apps Script backend, paste the latest /exec URL here.
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbxg5ns8L_vO4Z1A2OxnqkKDNaHb9_WDHSUdfPXX4FR7ly55yYCty3jROrtknPzkdSzv/exec',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbyhiyQjeoPw65FwqykknGGcbX8TwKIgLBrU935TrFTJmVBpBbTrfXYFgSR3L7LrIPzgMA/exec',
 
   CSI_SURVEY_URL: 'https://survey.nhealth-asia.com/s/cm1hci0mw00jf45vmps2myg1g',
 
